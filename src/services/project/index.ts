7721495bc9e6fafec360d220f38805df31789993
khaledssbd <khaledssbd@gmail.com>
2025-05-31 09:59:16 +0600

    Update fetch requests in project and skill services to include pagination and sorting parameters

--- src/services/project/index.ts
+++ src/services/project/index.ts
@@ -3,12 +3,15 @@
 // getAllProjects
 export const getAllProjects = async (): Promise<any> => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/project`, {
-      next: {
-        tags: ['PROJECTS'],
-        revalidate: 1800, // Revalidate this page every 1800 seconds // this is not needed if I use export const revalidate: 1800; inside the home page
-      },
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/project?limit=5000&page=1&sort=-createdAt`,
+      {
+        next: {
+          tags: ['PROJECTS'],
+          revalidate: 1800, // Revalidate this page every 1800 seconds // this is not needed if I use export const revalidate: 1800; inside the home page
+        },
+      }
+    );
 
     const result = await res.json();
     return result;
--- src/services/skill/index.ts
+++ src/services/skill/index.ts
@@ -3,12 +3,15 @@
 // getAllSkills
 export const getAllSkills = async (): Promise<any> => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/skill`, {
-      next: {
-        tags: ['SKILLS'],
-        revalidate: 1800, // Revalidate this page every 1800 seconds // this is not needed if I use export const revalidate: 1800; inside the home page
-      },
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/skill?limit=5000&page=1&sort=createdAt`,
+      {
+        next: {
+          tags: ['SKILLS'],
+          revalidate: 1800, // Revalidate this page every 1800 seconds // this is not needed if I use export const revalidate: 1800; inside the home page
+        },
+      }
+    );
 
     const result = await res.json();
     return result;
